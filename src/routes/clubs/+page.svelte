ccb1dfdec275104dd69c367fe3b34f7241c2a370
Sebastian Moritz <sebastian.moritz@mekmedia.com>
2026-01-16 18:34:04 +0100

    adding image to club

--- src/routes/clubs/+page.svelte
+++ src/routes/clubs/+page.svelte
@@ -2,8 +2,6 @@
   import Card from "$lib/ui/Card.svelte";
   import { subTitle } from "$lib/runes.svelte";
   import ClubForm from "./ClubForm.svelte";
-
-  subTitle.text = "Register your clubs";
 </script>
 <Card title="Register Club">
   <ClubForm />
