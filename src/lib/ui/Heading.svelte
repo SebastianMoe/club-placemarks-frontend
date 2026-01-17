e8b77a48c3fa5f7fb0f6bf7dfa471af6457a6cda
Sebastian Moritz <sebastian.moritz@mekmedia.com>
2026-01-17 14:20:54 +0100

    remove subText

--- src/lib/ui/Heading.svelte
+++ src/lib/ui/Heading.svelte
@@ -3,11 +3,4 @@
 </script>
 
 <section class="hero is-link is-small mt-6 mb-6">
-  <div class="hero-body">
-    <div class="container">
-      <h1 class="title has-text-centered">
-        {subTitle.text}
-      </h1>
-    </div>
-  </div>
 </section>
